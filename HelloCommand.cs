fd0714d9cf5f938f367c6b63bce1913ff2ffbe74
Harpreet Singh <61225147+SHarpreet89@users.noreply.github.com>
2025-10-22 11:24:31 +1100

    Update HelloCommand.cs

--- HelloCommand.cs
+++ HelloCommand.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Windows.Forms;
-using Autodesk.Revit.UI;
-using Autodesk.Revit.DB;
-
 namespace MyRevitAddin
 {
     public class HelloCommand : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            MessageBox.Show("Hello from ExternalCommand", "MyRevitAddin");
+            System.Windows.Forms.MessageBox.Show("Hello from ExternalCommand", "MyRevitAddin");
             return Result.Succeeded;
         }
     }
