ac4d1e487155861afa1eaae988f31f8765564b4d
Harpreet Singh <61225147+SHarpreet89@users.noreply.github.com>
2025-10-23 13:57:11 +1100

    Update HelloCommand.cs

--- HelloCommand.cs
+++ HelloCommand.cs
@@ -1,24 +1,23 @@
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Windows.Forms;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
-// 🔥 INLINE VERSION ATTRIBUTES (CRITICAL)
+// 🔥 Inline assembly attributes (these override AssemblyInfo.cs)
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
 
-namespace TESTPLUGIN
+namespace MyRevitAddin
 {
     [Transaction(TransactionMode.Manual)]
     public class HelloCommand : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            TaskDialog.Show("Hello", "Hello from Revit Add-in!");
+            TaskDialog.Show("Revit Add-In", "Hello from Revit 2025!");
             return Result.Succeeded;
         }
     }
