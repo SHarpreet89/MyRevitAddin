4c3bead2c5c1f53825c5a836fe52618b85f486bd
Harpreet Singh <61225147+SHarpreet89@users.noreply.github.com>
2025-10-23 13:48:27 +1100

    Update HelloCommand.cs

--- HelloCommand.cs
+++ HelloCommand.cs
@@ -1,8 +1,18 @@
 using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+
+// 🔥 INLINE ASSEMBLY ATTRIBUTES - Bypasses AssemblyInfo.cs issues!
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyTitle("TESTPLUGIN")]
+[assembly: AssemblyProduct("TESTPLUGIN")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a7d0e1f3-4b2c-4e5d-8f9a-1c3d5e6f7a8b")]
 
 namespace TESTPLUGIN
 {
@@ -16,7 +26,7 @@ namespace TESTPLUGIN
         {
             try
             {
-                TaskDialog.Show("TESTPLUGIN", "Hello from TESTPLUGIN!");
+                TaskDialog.Show("TESTPLUGIN", "Hello from TESTPLUGIN! Line 146 defeated!");
                 return Result.Succeeded;
             }
             catch (Exception ex)
